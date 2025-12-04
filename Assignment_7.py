b339aefc0295b3ed899462b8d1f3fea4a3f03ec1
sb80617 <sb80617@mail.harpercollege.edu>
2025-12-03 19:13:55 -0600

    Add files via upload

---
+++ Assignment_7.py
@@ -0,0 +1,21 @@
+class employee():                  
+    def __init__(self, first_name, last_name, salary, bonus = .10 ):
+        self.First_name = first_name
+        self.Last_name = last_name 
+        self.Salary = salary
+        self.Bonus = bonus
+class manager(employee):
+      def __init__(self, first_name, last_name, salary, bonus = .20 ):
+           self.First_name = first_name
+           self.Last_name = last_name 
+           self.Salary = salary
+           self.Bonus = bonus
+      def long_term(self):
+        self.Bonus = .50
+jim = manager("jim", "jones", 200000)
+print(jim.First_name)
+print(jim.Last_name)
+print(jim.Salary)
+print(jim.Bonus)
+jim.long_term()
+print(jim.Bonus)
---
+++ Assignment_7_problem_2.py
@@ -0,0 +1,37 @@
+from typing import Self
+
+
+class car():
+    def __init__(self, make, model, sticker_price, discount_price):
+        self.Make = make
+        self.Model = model 
+        self.Sticker_price = sticker_price
+        self.Discount_price = sticker_price * .9
+
+class sport():
+    def __init__(self, make, model, sticker_price, discount_price,option):
+        self.Make = make
+        self.Model = model 
+        self.Sticker_price = sticker_price
+        self.Discount_price = sticker_price * .9
+        self.Option = option
+    
+    def sportwheels(self):
+        self.Option += 1000
+
+    def sportinterior(self):
+        self.Option += 2000
+    
+    def sportengine(self):
+        self.Option += 3000
+mustang = sport("ford","mustang",20000,0,0)
+print(mustang.Make)
+print(mustang.Model)
+print(mustang.Sticker_price)
+print(mustang.Discount_price)
+mustang.sportengine()
+print(mustang.Option)
+mustang.sportinterior()
+print(mustang.Option)
+mustang.sportinterior()
+print(mustang.Option)
